2e579619d0b8c42322ba0a47da632d019cbf022d
toni <toniruiv@gmail.com>
2019-05-28 11:37:40 +0200

    create

---
+++ services - copia/users/users.service.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed } from '@angular/core/testing';
+import { UsersService } from './users.service';
+import { Observable, of } from 'rxjs';
+import * as faker from 'ng-faker';
+
+describe('UsersService', () => {
+  let usersService: UsersService; // Add this
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UsersService]dsdasdasdasdas
+    });
+
+    usersService = TestBed.get(UsersService); // Add this
+  });
+
+  it('should be created', () => { // Remove inject()
+    expect(usersService).toBeTruthy();
+  });
+  const oneHost = () => {
+    return {
+        id: faker.random.uuid(),
+        name: faker.random.String(),
+        role: faker.random.String(),
+        pokemon: faker.random.String(),
+    };
+};
+  describe('all', () => {
+    it('should return a collection of users', () => {
+      const userResponse = oneHost;
+      let response;
+      spyOn(usersService, 'all').and.returnValue(of(userResponse));
+
+      usersService.all().subscribe(res => {
+        response = res;
+      });
+
+      expect(response).toEqual(userResponse);
+    });
+  });
+  describe('findOne', () => {
+    it('should return a single user', () => {
+      const userResponse = oneHost;
+      let response;
+      spyOn(usersService, 'findOne').and.returnValue(of(userResponse));
+  
+      usersService.findOne('2').subscribe(res => {
+        response = res;
+      });
+  
+      expect(response).toEqual(userResponse);
+    });
+  });
+});
